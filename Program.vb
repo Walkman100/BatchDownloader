f89acab1467f598ca501d6b2934842910042023a
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2016-03-29 17:20:51 +0200

    Added project files

---
+++ Program.vb
@@ -0,0 +1,16 @@
+﻿'
+' Created by SharpDevelop.
+' User: Walkman
+' Date: 2016/03/29
+' Time: 05:20 PM
+'
+Module Program
+    Sub Main()
+        Console.WriteLine("Hello World!")
+        
+        ' TODO: Implement Functionality Here
+        
+        Console.Write("Press any key to continue . . . ")
+        Console.ReadKey(True)
+    End Sub
+End Module
